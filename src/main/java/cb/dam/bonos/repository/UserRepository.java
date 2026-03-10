e18965f7f495e9d3885a388b1d3f986035985e07
Christian Bihurriet <christian.00@hotmail.com.ar>
2026-03-10 14:09:54 +0000

    refactor: creation of entity Bono and BonoRepository

--- src/main/java/cb/dam/bonos/repository/UserRepository.java
+++ src/main/java/cb/dam/bonos/repository/UserRepository.java
@@ -6,9 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
+    boolean existsByUsername(String username);
 }
